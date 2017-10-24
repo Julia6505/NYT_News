e320b515489c3e845ac266371f626b073a617fa7
Julia McGee <JuliaMcGee@Kenneths-MacBook-Pro.local>
2017-10-24 14:32:05 -0400

    heroku

--- server.js
+++ server.js
@@ -45,7 +45,7 @@ mongoose.Promise = Promise;
 if (process.env.MONGODB_URI){
   mongoose.connect(process.env.MONGODB_URI)
 }else{
-mongoose.connect("mongodb://nytscrape:scrape@ds227865.mlab.com:27865/heroku_5556xmfk", {
+mongoose.connect("mongodb://localhost/nytscrape", {
   useMongoClient: true
 });
 };
